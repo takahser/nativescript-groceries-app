bef9ab21577568957ac684f3c99e6c866c8c41d8
Seraya Takahashi <seraya.takahashi@softfactors.com>
2017-12-28 13:02:05 +0100

    added finished groceries project

---
+++ app/shared/config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Config = /** @class */ (function () {
+    function Config() {
+    }
+    Config.apiUrl = "https://api.everlive.com/v1/GWfRtXi1Lwt4jcqK/";
+    Config.token = "";
+    return Config;
+}());
+exports.Config = Config;
